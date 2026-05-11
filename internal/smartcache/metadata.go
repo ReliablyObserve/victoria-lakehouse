8fa5e6a59fd364624a5cae4d9e37b210dd0cfdd8
szibis <szibis@users.noreply.github.com>
2026-05-11 18:16:27 +0200

    fix: use 0600 file permissions for cache snapshot (gosec G306)

--- internal/smartcache/metadata.go
+++ internal/smartcache/metadata.go
@@ -158,7 +158,7 @@ func (m *MetadataMap) SaveSnapshot(path string) error {
 	}
 
 	tmpPath := path + ".tmp"
-	if err := os.WriteFile(tmpPath, data, 0644); err != nil {
+	if err := os.WriteFile(tmpPath, data, 0600); err != nil {
 		return err
 	}
 	return os.Rename(tmpPath, path)
